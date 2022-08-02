560a62a4a4bc0f801d4feef4f1d929fdc2f8e038
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-02 10:31:53 +0200

    scss- create style btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
-let main = document.createElement('main');
-document.querySelector('body').appendChild(main);
+const calculator = ['(',')','%','AC','7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+']
 
-let divEcran = document.createElement('div')
-main.appendChild(divEcran)
+let main = document.createElement("main");
+document.querySelector("body").appendChild(main);
 
+/**Create Ecran Output */
+let divOutput = document.createElement("div");
+main.appendChild(divOutput);
+divOutput.classList.add('output');
+let divPrevious = document.createElement("div");
+divOutput.appendChild(divPrevious);
+divPrevious.classList.add('previous');
+let divCurrent = document.createElement("div");
+divOutput.appendChild(divCurrent);
+divCurrent.classList.add('current');
 
-    for (let i = 0; i <= 9; i++) {
-        let button = document.createElement('button');
-        let buttonText = document.createTextNode(i);
-        button.appendChild(buttonText)
-        main.appendChild(button);
-    }
+/**Create button */
+for (let i = 0; i < calculator.length; i++) {
+  let btn = document.createElement("button");
+  let btnText = document.createTextNode(calculator[i]);
+  btn.appendChild(btnText);
+  main.appendChild(btn);
+  btn.classList.add('btnStyle', 'btn'+i);
+}
