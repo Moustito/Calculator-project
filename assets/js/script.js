d928d1bdb7b34242a47f841b99b465abb2202677
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-02 14:15:03 +0200

    js- create concatenation + AC reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
-const calculator = ['(',')','%','AC','7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+']
+//CALCULATOR
+const calculator = [
+  "(",
+  ")",
+  "%",
+  "AC",
+  "7",
+  "8",
+  "9",
+  "/",
+  "4",
+  "5",
+  "6",
+  "*",
+  "1",
+  "2",
+  "3",
+  "-",
+  "0",
+  ".",
+  "=",
+  "+",
+];
 
 let main = document.createElement("main");
 document.querySelector("body").appendChild(main);
@@ -6,13 +28,15 @@ document.querySelector("body").appendChild(main);
 /**Create Ecran Output */
 let divOutput = document.createElement("div");
 main.appendChild(divOutput);
-divOutput.classList.add('output');
+divOutput.id = "output";
+
 let divPrevious = document.createElement("div");
 divOutput.appendChild(divPrevious);
-divPrevious.classList.add('previous');
+divPrevious.id = "previous";
+
 let divCurrent = document.createElement("div");
 divOutput.appendChild(divCurrent);
-divCurrent.classList.add('current');
+divCurrent.id = "current";
 
 /**Create button */
 for (let i = 0; i < calculator.length; i++) {
@@ -20,5 +44,39 @@ for (let i = 0; i < calculator.length; i++) {
   let btnText = document.createTextNode(calculator[i]);
   btn.appendChild(btnText);
   main.appendChild(btn);
-  btn.classList.add('btnStyle', 'btn'+i);
+  btn.classList.add("btnStyle", "btn" + i);
+
+  btn.id = calculator[i];
+}
+
+// concatenation in previous
+for (let element of calculator) {
+  let concatenate = document
+    .getElementById(element)
+    .addEventListener("click", (event) => {
+      if (!(element === 'AC')){
+        displayNum(element);
+      }
+    });
+}
+var displayString = "";
+function displayNum(element) {
+  let input = document.getElementById(element).textContent;
+  let displayInputInOutput = document.getElementById("previous");
+  displayString = displayString + input;
+  displayInputInOutput.innerHTML = displayString;
+
+}
+
+// AC
+// document.getElementById('AC').removeEventListener("click", displayNum(element))
+  let concatenateReset = document
+    .getElementById('AC')
+    .addEventListener("click", (event) => {
+      fuckingReset();
+    });
+
+function fuckingReset() {
+  document.getElementById('previous').innerHTML = ''
+  displayString = ''
 }
