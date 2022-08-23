7697d4a53b0b5ed9d90cc84cf1d7a7211939d281
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-23 10:57:02 +0200

    refactor function

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,52 +22,57 @@ const calculator = [
   "+",
 ];
 
-let main = document.createElement("main");
-document.querySelector("body").appendChild(main);
+function createDomEcran() {
+  let main = document.createElement("main");
+  document.querySelector("body").appendChild(main);
 
-/**Create Ecran Output */
-let divOutput = document.createElement("div");
-main.appendChild(divOutput);
-divOutput.id = "output";
+  /**Create Ecran Output */
+  let divOutput = document.createElement("div");
+  main.appendChild(divOutput);
+  divOutput.id = "output";
 
-let divPrevious = document.createElement("div");
-divOutput.appendChild(divPrevious);
-divPrevious.id = "previous";
+  let divPrevious = document.createElement("div");
+  divOutput.appendChild(divPrevious);
+  divPrevious.id = "previous";
 
-let divCurrent = document.createElement("div");
-divOutput.appendChild(divCurrent);
-divCurrent.id = "current";
+  let divCurrent = document.createElement("div");
+  divOutput.appendChild(divCurrent);
+  divCurrent.id = "current";
 
-/**Create button */
-for (let i = 0; i < calculator.length; i++) {
-  let btn = document.createElement("button");
-  let btnText = document.createTextNode(calculator[i]);
-  btn.appendChild(btnText);
-  main.appendChild(btn);
-  btn.classList.add("btnStyle", "btn" + i);
+  /**Create button */
+  for (let i = 0; i < calculator.length; i++) {
+    let btn = document.createElement("button");
+    let btnText = document.createTextNode(calculator[i]);
+    btn.appendChild(btnText);
+    main.appendChild(btn);
+    btn.classList.add("btnStyle", "btn" + i);
 
-  btn.id = calculator[i];
+    btn.id = calculator[i];
+  }
 }
 
 // concatenation in previous
-for (let element of calculator) {
-  let concatenate = document
-    .getElementById(element)
-    .addEventListener("click", (event) => {
-      switch (element) {
-        case "AC":
-          concatenationReset();
-          break;
-        case "=":
-          concatenationResult();
-          displayNum(element);
-          break;
-        default:
-          displayNum(element);
-          break;
-      }
-    });
+function concatenationInInput() {
+  for (let element of calculator) {
+    let concatenate = document
+      .getElementById(element)
+      .addEventListener("click", (event) => {
+        switch (element) {
+          case "AC":
+            concatenationReset();
+            break;
+          case "=":
+            concatenationResult();
+            displayNum(element);
+            break;
+          default:
+            displayNum(element);
+            break;
+        }
+      });
+  }
 }
+
 var displayString = "";
 function displayNum(element) {
   let input = document.getElementById(element).textContent;
@@ -93,74 +98,77 @@ function computeResult(str) {
   return Function("return " + str)();
 }
 
-
-
-
 //Button handle
-document.addEventListener("keyup", (event) => {
-  switch (event.key) {
-    //operator
-    case "Enter":
-      concatenationResult();
-      displayNum("=");
-      break;
-    case "+":
-      displayNum("+");
-      break;
-    case "-":
-      displayNum("-");
-      break;
-    case "*":
-      displayNum("*");
-      break;
-    case "/":
-      displayNum("/");
-      break;
-    case "%":
-      displayNum("%");
-      break;
-    case "(":
-      displayNum("(");
-      break;
-    case ")":
-      displayNum(")");
-      break;
-    case ".":
-      displayNum(".");
-      break;
-    case "Backspace":
-      concatenationReset();
-      break;
-    //number
-    case "0":
-      displayNum("0");
-      break;
-    case "1":
-      displayNum("1");
-      break;
-    case "2":
-      displayNum("2");
-      break;
-    case "3":
-      displayNum("3");
-      break;
-    case "4":
-      displayNum("4");
-      break;
-    case "5":
-      displayNum("5");
-      break;
-    case "6":
-      displayNum("6");
-      break;
-    case "7":
-      displayNum("7");
-      break;
-    case "8":
-      displayNum("8");
-      break;
-    case "9":
-      displayNum("9");
-      break;
-  }
-});
+function buttonHandle() {
+  document.addEventListener("keyup", (event) => {
+    switch (event.key) {
+      //operator
+      case "Enter":
+        concatenationResult();
+        displayNum("=");
+        break;
+      case "+":
+        displayNum("+");
+        break;
+      case "-":
+        displayNum("-");
+        break;
+      case "*":
+        displayNum("*");
+        break;
+      case "/":
+        displayNum("/");
+        break;
+      case "%":
+        displayNum("%");
+        break;
+      case "(":
+        displayNum("(");
+        break;
+      case ")":
+        displayNum(")");
+        break;
+      case ".":
+        displayNum(".");
+        break;
+      case "Backspace":
+        concatenationReset();
+        break;
+      //number
+      case "0":
+        displayNum("0");
+        break;
+      case "1":
+        displayNum("1");
+        break;
+      case "2":
+        displayNum("2");
+        break;
+      case "3":
+        displayNum("3");
+        break;
+      case "4":
+        displayNum("4");
+        break;
+      case "5":
+        displayNum("5");
+        break;
+      case "6":
+        displayNum("6");
+        break;
+      case "7":
+        displayNum("7");
+        break;
+      case "8":
+        displayNum("8");
+        break;
+      case "9":
+        displayNum("9");
+        break;
+    }
+  });
+} 
+
+createDomEcran();
+concatenationInInput();
+buttonHandle();
